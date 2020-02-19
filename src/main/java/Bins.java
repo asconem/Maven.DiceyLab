34ec9673ad01e4577776cc0a780b4c1556dfde07
asconem <asconem@gmail.com>
2020-02-19 16:43:03 -0500

    WIP

--- src/main/java/Bins.java
+++ src/main/java/Bins.java
@@ -1,4 +1,5 @@
+import java.util.ArrayList;
 
 public class Bins {
-
+    ArrayList<Integer> bin = new ArrayList<>();
 }
---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+public class DiceTest {
+
+    @Test
+    public void diceTest() {
+        Dice dice = new Dice(2);
+
+    }
+
+}
